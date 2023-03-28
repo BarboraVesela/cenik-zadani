6ed974c68044aac7f219b52219d6288b57e67237
Barbora Vesela <barbora.vesela@booking.com>
2023-03-28 21:40:44 +0200

    Oprava

--- index.js
+++ index.js
@@ -3,22 +3,21 @@ const selectPlan = (planNumber) => {
     let plan2Elm = document.querySelector('#plan2');
     let plan3Elm = document.querySelector('#plan3');
     
-    if (plan1Elm.selected == True){
+    if (planNumber === 1 ){
         plan1Elm.classList.add("plan--selected")
-    }else if (plan1Elm.selected == False){
+    }else if (plan1Elm.selected != 1){
         plan1Elm.classList.remove("plan--selected")
     }
 
-    if (plan2Elm.selected == True){
+    if (planNumber === 2){
         plan2Elm.classList.add("plan--selected")
-    }else if (plan2Elm.selected == False){
+    }else if (plan2Elm.selected != 2){
         plan2Elm.classList.remove("plan--selected")
     }
 
-    if (plan3Elm.selected == True){
+    if (planNumber === 3){
         plan3Elm.classList.add("plan--selected")
-    }else if (plan3Elm.selected == False){
+    }else if (plan3Elm.selected != 3){
         plan3Elm.classList.remove("plan--selected")
     }
 }
-    
